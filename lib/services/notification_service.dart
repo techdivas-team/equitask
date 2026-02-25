48846940f5f6bf3c42ad1d62403dcd809d101bb3
Nyaraih-Tok <nyaraihk@gmail.com>
2026-02-25 23:29:00 +0200

    add notifier app service

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,6 +1,19 @@
 import 'api_services.dart';
+import '../models/app_notification.dart';
 
 class NotificationService {
   final ApiService _apiService;
   NotificationService(this._apiService);
+
+  Future<List<AppNotification>> getNotifications() async {
+    final response = await _apiService.get('/notifications');
+    final notifications = (response['notifications'] as List<dynamic>? ?? []);
+    return notifications
+        .map((item) => AppNotification.fromJson(item as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> markAllRead() async {
+    await _apiService.post('/notifications/mark-all-read', {});
+  }
 }

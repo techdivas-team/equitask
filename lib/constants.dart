1d0787f68c96b218fd06bf580d8424cf1272aa02
Nyaraih-Tok <nyaraihk@gmail.com>
2026-03-02 20:10:55 +0200

    changed base url for remote server and notifications file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,12 @@
 const String apiBaseUrl = 'https://equitask-backend.onrender.com';
+
+// Web client ID (for web platform)
 const String googleWebClientId = String.fromEnvironment(
   'GOOGLE_WEB_CLIENT_ID',
   defaultValue:
       '995530463322-fqr9iqdk5mrf4lsbdua3bfqabbh2vi3k.apps.googleusercontent.com',
 );
+
+// Android client ID (for Android native app)
+const String googleAndroidClientId =
+    '995530463322-4lvdn83gcmj5957o6a0tim6v0lndqtu9.apps.googleusercontent.com';
--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -1,1 +1,35 @@
-export 'app_notification.dart';
+class NotificationModel {
+  final String id;
+  final String title;
+  final String body;
+  final DateTime createdAt;
+  final bool isRead;
+
+  NotificationModel({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.createdAt,
+    required this.isRead,
+  });
+
+  factory NotificationModel.fromJson(Map<String, dynamic> json) {
+    return NotificationModel(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      createdAt: DateTime.parse(json['createdAt']),
+      isRead: json['isRead'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'createdAt': createdAt.toIso8601String(),
+      'isRead': isRead,
+    };
+  }
+}
